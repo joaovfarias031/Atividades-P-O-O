a1c7bd40588387839ca630625750db6250f3cdd5
João Victor Farias <joao.mirandafarias16@gmail.com>
2026-06-10 16:08:45 -0300

    Update questao_03_metodos_mágicos.py

--- questao_03_metodos_mágicos.py
+++ questao_03_metodos_mágicos.py
@@ -8,12 +8,24 @@ class carteira:
             return (self.saldo * 0.14) + valor_yuan
         elif self.moeda == "BRL":     
             return (self.saldo * 0.85) + valor_yuan
+        else:
+            return self.saldo + valor_yuan
 
     def __sub__(self, valor_yuan):
         if self.moeda == "USD":
             return (self.saldo * 0.14) - valor_yuan
         elif self.moeda == "BRL":     
             return (self.saldo * 0.85) - valor_yuan
+        else:
+            return self.saldo - valor_yuan
 
-carteira_usd = carteira("USD", 10.0)
+carteira_usd = carteira("USD", 100.0)
+carteira_BRL = carteira("BRL", 200.0)
+carteira_CNY = carteira("CNY", 300.0)
 print("soma de carteira USD + 50 yuan =", carteira_usd + 50.0)
+print("soma de carteira BRL + 50 yuan =", carteira_BRL + 50.0)
+print("soma de carteira CNY + 50 yuan =", carteira_CNY + 50.0)
+print("subtração de carteira USD + 50 yuan =", carteira_usd - 50.00)
+print("subtração  de carteira BRL + 50 yuan =", carteira_BRL - 50.0)
+print("subtração  de carteira CNY + 50 yuan =", carteira_CNY - 50.0)
+
