6eb4cc15139c1a5d5dec29fbde21daadb25ac5be
João Victor Farias <joao.mirandafarias16@gmail.com>
2026-06-15 10:18:01 -0300

    Update questao_05_metodos_magicos.py

--- questao_05_metodos_magicos.py
+++ questao_05_metodos_magicos.py
@@ -8,12 +8,38 @@ class onibus:
         return self.len(self.assentos)
 
     def __getitem__(self, indice):
-        pass
+        if indice >= 0 or indice < len(self.assentos):
+            if self.assentos[indice] == False:
+                return self.assentos[indice] 
+            elif self.assentos[indice] == True:
+                return self.assentos[indice] 
+        else:
+            raise IndexError(f"Escolha um valor entre 0 e {len(self.assentos)}")
+
 
     def __setitem__(self, indice, valor):
-        pass
+        if indice >= 0 or indice < len(self.assentos):
+            if isinstance(valor, bool):
+                if self.assentos[indice] == False:
+                    return valor
+                elif self.assentos[indice] == True:
+                    return valor
+            else:
+                raise TypeError(f"Valor deve ser booleano (True/False)")
+        else:
+            raise IndexError(f"Escolha um valor entre 0 e {len(self.assentos)}")
+         
+
 
     def __str__(self):
-        return f"Ônibus (Placa: {self.placa}) Motorista: {self.nome_motorista}"
-        return f"Assentos totais: {self.assentos}"
-                 
+        txt = f"Ônibus (Placa: {self.placa}) Motorista: {self.nome_motorista}"
+        txt += f"Assentos totais: {len(self.assentos)}"
+        txt += f"Assentos ocupados: {len(self.assentos == True)}"
+        txt += f"Assentos Livres: {len(self.assentos == False)}"
+        return txt
+
+onibus = onibus("ABC-1234", "João Silva", 10) # Ônibus com 10 assentos
+print(len(onibus)) # Verificando total de assentos 
+onibus[0] = True # Ocupando o primeiro assento (índice 0) 
+print(onibus) # Exibindo informações do ônibus 
+
