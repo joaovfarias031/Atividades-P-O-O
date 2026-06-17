941dc28f0a5af8436b017ba4b5e2d67505c6f0af
João Victor Farias <joao.mirandafarias16@gmail.com>
2026-06-17 17:04:57 -0300

    Update questao_06_metodos_magicos.py

--- questao_06_metodos_magicos.py
+++ questao_06_metodos_magicos.py
@@ -6,4 +6,125 @@ class Personagem:
     def tomar_dano(valor):
         self.vida -= valor
 
+class Mago(Personagem):
+    def __init__(self):
+        super.__init__(nome, vida)
+        self.mana = 10
+
+    def __str__(self):
+        return f"nome: {self.nome}, vida: {self.vida}, mana: {self.mana}"
+
+    def __add__(self, valor: float):
+        self.mana += valor
+        return self.mana
+
+    def __sub__(self, valor: float):
+        self.mana -= valor
+        if self.mana < 0:
+            self.mana = 0
+        return self.mana
+
+    def __mul__(self, fator: float):
+        self.mana *= fator 
+        return self.mana
+
+    def __truediv__(Self, valor: float):
+        self.mana %= valor
+        return self.mana
+
     
+class Barbaro(Personagem):
+    def __init__(self):
+        super.__init__(nome, vida)
+        self.stamina = 10
+        self.fúria = False
+
+    def __str__(self):
+        return f"nome: {self.nome}, vida: {self.vida}, stamina: {self.stamina}, fúria: {self.furia}"
+
+    def __add__(self, valor: float):
+        self.stamina += valor
+        if self.furia == True:
+            self.stamina *= 1.5
+            return self.stamina
+        else:
+            return self.stamina
+
+        
+    def __sub__(self, valor: float):
+        self.stamina -= valor
+        if self.stamina == 0 and not self.furia == True:
+            self.furia = True
+            self.stamina = 5
+            return self.furia
+            return self.stamina
+
+        else:
+            return self.stamina
+
+    def __mul__(self, fator: float):
+        self.stamina *= fator
+        return self.stamina
+        if self.furia == True:
+            self.vida += 5
+            return self.vida
+
+    def __truediv__(self, valor: float):
+        self.stamina %= valor
+        return self.stamina
+
+
+nome = input("Digite o nome do personagem: ")
+vida = input("Digite a vida do personagem: ")
+tipo  = input("Digite o tipo do personagem (Mago ou Bárbaro): ")
+if tipo == 'Mago':
+    Pm = Mago(nome, vida)
+    While True:
+        print("Escolha uma ação:")
+        print("(1)Tomar poção simples")
+        print("(2) Tomar poção especial")
+        print("(3) ataque básico")
+        print("(4) ataque especial")
+        escolha = input("Digite o número da ação: ")
+        if escolha == "1":
+            Pm + 5
+        elif escolha == "2":
+            Pm * 1.5
+        elif escolha == "3":
+            Pm - 2 
+        elif escolha == "4":
+            Pm / 2
+        valor_dano = random.randint(1, 10)
+        Pm.tomar_dano(valor_dano)
+        if Pm.self.vida == 0:
+            break
+        
+
+elif tipo == 'Barbaro':
+    Pb = Barbaro(nome, vida)
+    While True:
+        print("Escolha uma ação:")
+        print("(1)Tomar poção simples")
+        print("(2) Tomar poção especial")
+        print("(3) ataque básico")
+        print("(4) ataque especial")
+        
+        escolha = input("Digite o número da ação: ")
+        if escolha == "1":
+            Pb + 5
+        elif escolha == "2":
+            Pb * 1.5
+        elif escolha == "3":
+            Pb - 2 
+        elif escolha == "4":
+            Pb / 2
+        valor_dano = random.randint(1, 10)
+        Pb.tomar_dano(valor_dano)
+        if Pb.self.vida == 0:
+            break
+
+
+
+    
+
+        
