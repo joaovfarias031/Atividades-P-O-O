26837994e2259117fd5d6920c9c9e3b08b0698a8
João Victor Farias <joao.mirandafarias16@gmail.com>
2026-06-25 14:48:49 -0300

    Update dicionario.py

--- dicionario.py
+++ dicionario.py
@@ -13,7 +13,19 @@ frase = "python programming"
 resultado = contagem_caracteres(frase)
 print(resultado)
 
+#Questao_02
+quant_palavras = {}
+with open('estomago.txt', 'r') as arquivo:
+    linha = arquivo.readline()
+    for palavra in linha.split():
+        if palavra in contagem:
+            quant_palavras[palavra] += 1
+        else:
+            quant_palavras[palavras] = 1
+print(sorted(quant_palavras.values()))
+
 #Questao_03
 def mesclar_dicionarios(dict_1, dict_2):
-    resultado = dict_1 | dict_2 
+    #resultado = dict_1 | dict_2 
+    pass
     
